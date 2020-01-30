2467e6dfcf94a63f32445497c6a189780708750b
Vlad <bladetheblade12@gmail.com>
2020-01-30 21:30:27 +0200

    Validation
    Done login Validation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -7,6 +8,8 @@ import "materialize-css/dist/js/materialize.min";
 import dateFilter from "./filters/flter.date";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 Vue.filter("date", dateFilter);
 
